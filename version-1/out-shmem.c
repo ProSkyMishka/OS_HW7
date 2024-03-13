154a97d7bfbb4cc33478ac1f51338f20eabe594f
ProSkyMishka <145161036+ProSkyMishka@users.noreply.github.com>
2024-03-13 23:06:41 +0300

    Add files via upload

---
+++ version-1/out-shmem.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <signal.h>
+
+typedef struct {
+    int number;
+} Data;
+
+int client_pid;
+int shm_id;
+char gen_object[] = "gen-memory";
+
+void client_signal_handler(int signal) {
+    if (signal == SIGINT) {
+        printf("Received SIGINT in the server. Sending SIGINT to client...\n");
+        kill(client_pid, SIGINT);
+    }
+    
+    if(shm_id != -1) {
+        shm_unlink(gen_object);
+        close(shm_id);
+    }
+    
+    exit(0);
+}
+
+int main() {
+    printf("Out PID: %d\n", getpid());
+    printf("Gen PID: ");
+    scanf("%d", &client_pid);  // Получить PID out от пользователя
+    
+    signal(SIGINT, client_signal_handler);
+    
+    //открыть объект
+    if ( (shm_id = shm_open(gen_object, O_RDWR, 0666)) == -1 ) {
+      printf("Opening error\n");
+      perror("shm_open");
+      return 1;
+    } else {
+      printf("Object is open: name = %s, id = 0x%x\n", gen_object, shm_id);
+    }
+
+    //получить доступ к памяти
+    Data *shared_data = (Data *)mmap(NULL, sizeof(Data), PROT_READ | PROT_WRITE, MAP_SHARED, shm_id, 0);
+    if (shared_data == MAP_FAILED) {
+      printf("Error getting pointer to shared memory\n");
+      return 1;
+    }
+    
+    while (1) {
+        sleep(1); // Подождать 1 секунду перед чтением числа
+        printf("Получено число: %d\n", shared_data->number);
+    }
+    
+    return 0;
+}
